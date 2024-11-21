1790ef135a0e677c29853f01c1a4a5227d9e81a9
DESKTOP-UF52VF9\raynics <boldthua0330@gmail.com>
2024-11-21 11:35:20 +0800

    加入專案檔案。

---
+++ 點餐機/Extension.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 點餐機
+{
+    internal static class Extension
+    {
+
+        public static int GetCharacters(this string input)
+        {
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsDigit(input[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static void MenuLayout(this FlowLayoutPanel input, string[] menu,EventHandler checkedChange,EventHandler valueChange)
+        {
+            for (int i = 0; i < menu.Length; i++)
+            {
+                FlowLayoutPanel panel = new FlowLayoutPanel() { Size = new Size(300,25)};
+                CheckBox checkBox = new CheckBox() { Text = menu[i] };
+                NumericUpDown numericUpDown = new NumericUpDown() { Size = new Size(80, 25) };
+                checkBox.CheckedChanged += checkedChange;
+                numericUpDown.ValueChanged += valueChange;
+                input.Controls.Add(panel);
+                panel.Controls.Add(checkBox);
+                panel.Controls.Add(numericUpDown);
+            }
+        }
+
+        public static void MenuLayout(this FlowLayoutPanel input, string[] checkList)
+        {
+            foreach (string str in checkList)
+            {
+                Label lbl = new Label() { Text = str, Size = new Size(75, 25) };
+                if (int.TryParse(str, out _))
+                {
+                    lbl.Text = str + "          ";
+                    lbl.TextAlign = ContentAlignment.TopRight;
+                }
+
+                input.Controls.Add(lbl);
+            }
+        }
+
+        public static void MenuLayout(this FlowLayoutPanel input, string divider)
+        {
+                Label lbl = new Label() { Text = divider, Size = new Size(300, 25) };
+                input.Controls.Add(lbl);
+        }
+
+        public static int CalculatePrice(this FlowLayoutPanel input)
+        {
+            int price = 0;
+            foreach (FlowLayoutPanel panel in input.Controls)
+            {
+                CheckBox checkbox = (CheckBox)panel.Controls[0];
+                int quantity = (int)((NumericUpDown)panel.Controls[1]).Value;
+                string[] temp = panel.Controls[0].Text.Split('$');
+                int itemPrice = int.Parse(temp[1]);
+                if (checkbox.Checked)
+                {
+                    price += (itemPrice* quantity);
+                }
+            }
+            return price;
+        }
+
+        public static List<string[]> OrderedList(this FlowLayoutPanel input)
+        {
+            int price = 0;
+            List<string[]> orderedDetail = new List<string[]>();
+            foreach (FlowLayoutPanel panel in input.Controls)
+            {
+                int quantity = (int)((NumericUpDown)panel.Controls[1]).Value;
+                if (quantity > 0)
+                {
+                    CheckBox checkbox = (CheckBox)panel.Controls[0];
+
+                    string[] temp = panel.Controls[0].Text.Split('$');
+                    int itemPrice = int.Parse(temp[1]);
+                    if (checkbox.Checked)
+                    {
+                        price = (itemPrice * quantity);
+                    }
+                    orderedDetail.Add(new string[] { temp[0], temp[1], quantity.ToString(), price.ToString() });
+
+                }
+            }
+            return orderedDetail;
+        }
+    }
+}
---
+++ 點餐機/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 點餐機
+{
+    public partial class Form1 : Form
+    {
+        int priceSum = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string words = "Hello123World456";
+            Console.WriteLine(words.GetCharacters());
+            
+            
+            string[] mainDiet = { "招牌便當$65", "鯖魚便當$85", "雞腿飯$90", "雞翅便當$95", "豬腳飯$120", "鰻魚飯$180" };
+            string[] singelFood = { "鯖魚$60", "雞腿$65", "雞翅$60", "豬腳$80", "鰻魚$150" };
+            string[] drinks = { "紅茶$25", "奶茶$35", "豆漿$30","咖啡$35", "肥宅快樂水$40" };
+            string[] bites = { "雞塊$30", "薯條$40", "薯餅$45", "炸豆腐$40", "花枝丸$40", "毛豆$30" };
+
+            flowLayoutPanel1.MenuLayout(mainDiet, CheckedChange, ValueChange);
+            flowLayoutPanel2.MenuLayout(singelFood, CheckedChange, ValueChange);
+            flowLayoutPanel3.MenuLayout(drinks, CheckedChange, ValueChange);
+            flowLayoutPanel4.MenuLayout(bites, CheckedChange, ValueChange);
+
+            string[] checkList = {"品項", "單價", "數量", "小計"};
+            flowLayoutPanel5.MenuLayout(checkList);
+
+            string divider = "=================================================";
+            flowLayoutPanel5.MenuLayout(divider);
+
+        }
+
+        private void ShowDetail(List<string[]> foodLists)
+        {
+            foreach (string[] foodList in foodLists)
+            {
+                flowLayoutPanel5.MenuLayout(foodList);
+            }
+        }
+
+
+        private void CheckedChange(object sender,EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox) sender;
+            NumericUpDown numChange = (NumericUpDown)checkBox.Parent.Controls[1];
+            if (checkBox.Checked)
+            {
+                numChange.Value = 1;
+            }
+            else
+            {
+                numChange.Value = 0;
+            }            
+        }
+
+        private void ValueChange(object sender, EventArgs e)
+        {
+            NumericUpDown numChange = (NumericUpDown) sender;
+            CheckBox checkBox = (CheckBox)numChange.Parent.Controls[0];
+            if(numChange.Value == 0)
+            {
+                checkBox.Checked = false;
+
+            }
+            else
+            {
+                int temp = (int)numChange.Value;
+                checkBox.Checked = true;
+                numChange.Value = temp;
+            }
+            flowLayoutPanel5.Controls.Clear();
+            string[] checkList = { "品項", "單價", "數量", "小計" };
+            flowLayoutPanel5.MenuLayout(checkList);
+
+            string divider = "=================================================";
+            flowLayoutPanel5.MenuLayout(divider);
+
+            List<string[]> mainDietList = flowLayoutPanel1.OrderedList();
+            List<string[]> singelFoodList = flowLayoutPanel2.OrderedList();
+            List<string[]> drinksList = flowLayoutPanel3.OrderedList();
+            List<string[]> bitesList = flowLayoutPanel4.OrderedList();
+
+            ShowDetail(mainDietList);
+            ShowDetail(singelFoodList);
+            ShowDetail(drinksList);
+            ShowDetail(bitesList);
+
+            flowLayoutPanel5.MenuLayout(divider);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            priceSum = 0;
+            foreach (Control control in this.Controls)
+            {
+                if(control is FlowLayoutPanel panel && control.Tag == "menu")
+                {
+                    priceSum += panel.CalculatePrice();
+                }
+            }
+               
+            totalLab.Text = priceSum.ToString();
+        }
+
+    }
+}
